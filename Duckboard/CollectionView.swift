665a141e12f325fb7d05093420aaf2321de470d0
Rianne Pada <rianne.l.pada.ctr@apcss.dsca.mil>
2022-10-06 15:33:48 -1000

    finishing up

--- Duckboard/CollectionView.swift
+++ Duckboard/CollectionView.swift
@@ -10,13 +10,16 @@ import UIKit
 import CoreData
 
 class CollectionView: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
- 
+    
     var dataController: DataController!
     
+    @IBOutlet weak var DeletePhoto: UILabel!
     @IBOutlet weak var NoPhotosFound: UILabel!
     @IBOutlet weak var collectionPhotos: UICollectionView!
     
     override func viewDidLoad() {
+        self.DeletePhoto.isHidden = true
+        editButton()
         // https://knowledge.udacity.com/questions/906577
         favoritePhoto.sharedInstance().favePhoto = fetchFlickrPhotos()
         if favoritePhoto.sharedInstance().favePhoto.count > 0 {
@@ -27,7 +30,23 @@ class CollectionView: UIViewController, UICollectionViewDelegate, UICollectionVi
             self.NoPhotosFound.isHidden = false
         }
     }
+    // https://developer.apple.com/documentation/uikit/uiviewcontroller/1621378-setediting
+    override func setEditing(_ editing:Bool, animated:Bool){
+        super.setEditing(editing, animated: animated)
+        if (self.isEditing) {
+            self.editButtonItem.title = "Done"
+            self.DeletePhoto.isHidden = false
+        } else {
+            self.editButtonItem.title = "Edit"
+            self.DeletePhoto.isHidden = true
+        }
+    }
     
+    func editButton(){
+        // https://developer.apple.com/documentation/uikit/uiviewcontroller/1621471-editbuttonitem
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.navigationItem.rightBarButtonItem!.title = "Edit"
+    }
     // from https://stackoverflow.com/questions/24195310/how-to-add-an-action-to-a-uialertview-button-using-swift-ios
     
     // stack overflow said to use DispatchQueue: https://stackoverflow.com/questions/58087536/modifications-to-the-layout-engine-must-not-be-performed-from-a-background-thr
@@ -61,74 +80,83 @@ class CollectionView: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     // with help from Udacity mentor: https://knowledge.udacity.com/questions/906577
-        func downloadImage( imagePath:String, completionHandler: @escaping (_ imageData: Data?, _ errorString: String?) -> Void){
-                let session = URLSession.shared
-                let imgURL = NSURL(string: imagePath)
-                let request: NSURLRequest = NSURLRequest(url: imgURL! as URL)
-                
-                let task = session.dataTask(with: request as URLRequest) {data, response, downloadError in
-                    
-                    if downloadError != nil{
-                        completionHandler(nil, "Could not download image \(imagePath)")
-                    } else {
-                        completionHandler(data, nil)
-                    }
-                }
-                task.resume()
-            }
-    
-    
-
-        // MARK: Collection View Data Source
-        //from Udacity Lession 8.8 Setup the Sent Memes Collection View
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            favoritePhoto.sharedInstance().favePhoto.count
-        }
-        //from Udacity Lession 8.8 Setup the Sent Memes Collection View
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCellView", for: indexPath) as! ImageCellView
-            
-            let cellImage = favoritePhoto.sharedInstance().favePhoto[indexPath.row]
-            // with help from Udacity mentor: https://knowledge.udacity.com/questions/906577
-            // from Udacity project review https://review.udacity.com/#!/reviews/3735670
-            if cellImage.coreImage == nil{
-                let url = URL(string: cellImage.coreURL ?? "")
-                downloadImage(imagePath: url!.absoluteString) {(data, error) in
-                    DispatchQueue.main.async{
-                        cell.PhotoCell.image = UIImage(data: data!)
-                    }
-                    cellImage.coreImage = data
-                    try! self.dataController.viewContext.save()
-                }
-            } else {
-                DispatchQueue.main.async{
-                cell.PhotoCell.image = UIImage(data: cellImage.coreImage!)
-                }
-            }
-            return cell
-            
-        }
+    func downloadImage( imagePath:String, completionHandler: @escaping (_ imageData: Data?, _ errorString: String?) -> Void){
+        let session = URLSession.shared
+        let imgURL = NSURL(string: imagePath)
+        let request: NSURLRequest = NSURLRequest(url: imgURL! as URL)
         
-        // from Meme Me 2.0
-        // https://stackoverflow.com/questions/38028013/how-to-set-uicollectionviewcell-width-and-height-programmatically
-        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
-        {
-            return CGSize(width: 100.0, height: 100.0)
+        let task = session.dataTask(with: request as URLRequest) {data, response, downloadError in
+            
+            if downloadError != nil{
+                completionHandler(nil, "Could not download image \(imagePath)")
+            } else {
+                completionHandler(data, nil)
+            }
         }
-        // with help from Udacity mentor https://knowledge.udacity.com/questions/848663
-        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-            return 2
+        task.resume()
+    }
+    
+    // MARK: Collection View Data Source
+    //from Udacity Lession 8.8 Setup the Sent Memes Collection View
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        favoritePhoto.sharedInstance().favePhoto.count
+    }
+    //from Udacity Lession 8.8 Setup the Sent Memes Collection View
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCellView", for: indexPath) as! ImageCellView
+        
+        let cellImage = favoritePhoto.sharedInstance().favePhoto[indexPath.row]
+        // with help from Udacity mentor: https://knowledge.udacity.com/questions/906577
+        // from Udacity project review https://review.udacity.com/#!/reviews/3735670
+        if cellImage.coreImage == nil{
+            let url = URL(string: cellImage.coreURL ?? "")
+            downloadImage(imagePath: url!.absoluteString) {(data, error) in
+                DispatchQueue.main.async{
+                    cell.PhotoCell.image = UIImage(data: data!)
+                }
+                cellImage.coreImage = data
+                try! self.dataController.viewContext.save()
+            }
+        } else {
+            DispatchQueue.main.async{
+                cell.PhotoCell.image = UIImage(data: cellImage.coreImage!)
+            }
         }
-        func collectionView(_collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-            return 2
-        }
+        return cell
+        
+    }
+    
+    // from Meme Me 2.0
+    // https://stackoverflow.com/questions/38028013/how-to-set-uicollectionviewcell-width-and-height-programmatically
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        return CGSize(width: 100.0, height: 100.0)
+    }
+    // with help from Udacity mentor https://knowledge.udacity.com/questions/848663
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
+    }
+    func collectionView(_collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
+    }
     // https://developer.apple.com/documentation/uikit/uicollectionviewdelegate/1618032-collectionview
     // https://stackoverflow.com/questions/51526703/didselectitemat-and-diddeselectitemat-from-uicollectionview
+    //https://developer.apple.com/documentation/uikit/uiviewcontroller/1621498-isediting
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if isEditing{
             self.dataController.viewContext.delete(favoritePhoto.sharedInstance().favePhoto[indexPath.row])
-        favoritePhoto.sharedInstance().favePhoto.remove(at: indexPath.row)
+            favoritePhoto.sharedInstance().favePhoto.remove(at: indexPath.row)
             self.collectionPhotos.reloadData()
             try? dataController.viewContext.save()
+        } else {
+            print("Photo tapped")
+            let collectionController = self.storyboard?.instantiateViewController(withIdentifier: "IndividualViewPhoto") as! IndividualViewPhoto
+            collectionController.dataController = self.dataController
+            let flickrPhoto = favoritePhoto.sharedInstance().favePhoto[indexPath.row].coreURL
+            print(flickrPhoto)
+            collectionController.selectedImage = flickrPhoto
+            self.navigationController?.pushViewController(collectionController, animated: true)
         }
-    
+        
+    }
 }
---
+++ Duckboard/IndividualViewPhoto.swift
@@ -0,0 +1,49 @@
+//
+//  IndividualViewPhoto.swift
+//  Duckboard
+//
+//  Created by Brenna Pada on 10/6/22.
+//
+
+import Foundation
+import UIKit
+
+class IndividualViewPhoto: UIViewController {
+    
+    @IBOutlet weak var ShareImage: UIButton!
+    var dataController: DataController!
+    var selectedImage: String!
+    
+    // from https://stackoverflow.com/questions/24195310/how-to-add-an-action-to-a-uialertview-button-using-swift-ios
+    
+    // stack overflow said to use DispatchQueue: https://stackoverflow.com/questions/58087536/modifications-to-the-layout-engine-must-not-be-performed-from-a-background-thr
+    func showAlertAction(title: String, message: String){
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: {(action:UIAlertAction!) in
+                print("Action")
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    @IBOutlet weak var IndividualPhoto: UIImageView!
+    
+    override func viewDidLoad() {
+        showPhoto()
+    }
+    
+    func showPhoto(){
+        if let url = URL(string: selectedImage ?? "") {
+             let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                 guard let data = data, error == nil else { return }
+                 
+                 DispatchQueue.main.async { /// execute on main thread
+                     self.IndividualPhoto.image = UIImage(data: data)
+                 }
+             }
+             task.resume()
+         }
+    }
+    
+}
